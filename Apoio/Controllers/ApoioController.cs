d859ca3adbb8b24afa67c27b03044baa026b31ae
LuizHenriqueBarbosaDaSilva <luizbarbosa785@gmail.com>
2024-10-22 19:10:50 -0300

    Adcionado ApoioController - Empresa em html

---
+++ Apoio/Controllers/ApoioController.cs
@@ -0,0 +1,15 @@
+﻿using Apoio.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Apoio.Controllers
+{
+    public class ApoioController : Controller
+    {
+        public IActionResult Empresa()
+        {
+            ViewData["Title"] = "Controle de Empresas";
+            return View();
+        }
+    }
+}
--- Apoio/Program.cs
+++ Apoio/Program.cs
@@ -21,7 +21,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "Empresa",
+    pattern: "{controller=Apoio}/{action=Empresa}/{id?}");
 
 app.Run();
